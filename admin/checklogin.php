ef8995316d481499ff47d3cedef23f890c6c36fe
ChandulaSenevirathna <97549694+ChandulaSenevirathna@users.noreply.github.com>
2023-02-11 16:49:18 +0530

    Update checklogin.php

--- admin/checklogin.php
+++ admin/checklogin.php
@@ -1,5 +1,12 @@
 <?php
-$ip = md5($_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']);
+//$ip = md5($_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']);
+
+$options = [
+    'cost' => 12,
+];
+$ip = password_hash($_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT'], PASSWORD_BCRYPT, $options);
+
+
 $lastlogin = file_get_contents('lastlogin.encrypt');
 if($ip == $lastlogin){
 }
